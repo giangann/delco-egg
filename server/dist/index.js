4dac2969cd803f081eee81a16f6f8165db59f2bb
giangann <70595717+giangann@users.noreply.github.com>
2024-02-05 16:32:34 +0700

    fix build

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,38 +1,68 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step(
+        (generator = generator.apply(thisArg, _arguments || [])).next(),
+      );
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 require('dotenv').config();
-require("reflect-metadata");
-const typeorm_1 = require("typeorm");
-const logger_config_1 = __importDefault(require("./configs/logger.config"));
-const express_config_1 = __importDefault(require("./configs/express.config"));
+require('reflect-metadata');
+const typeorm_1 = require('typeorm');
+const logger_config_1 = __importDefault(
+  require('./configs/logger.config'),
+);
+const express_config_1 = __importDefault(
+  require('./configs/express.config'),
+);
 const PORT = process.env.PORT || 5000;
-const connect = () => __awaiter(void 0, void 0, void 0, function* () {
+const connect = () =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const connectionTest = typeorm_1.createConnection()
-            .then((value) => console.log(value))
-            .catch((e) => console.log(e));
-        const connection = yield typeorm_1.createConnection(); // Connect to the DB that is setup in the ormconfig.js
-        yield connection.runMigrations(); // Run all migrations
-        logger_config_1.default.info('Connect to database successfully');
-        express_config_1.default.listen(PORT, () => {
-            logger_config_1.default.info(`Server running at ${PORT}`);
-        });
+      const connection = yield typeorm_1.createConnection(); // Connect to the DB that is setup in the ormconfig.js
+      yield connection.runMigrations(); // Run all migrations
+      logger_config_1.default.info('Connect to database successfully');
+      express_config_1.default.listen(PORT, () => {
+        logger_config_1.default.info(`Server running at ${PORT}`);
+      });
+    } catch (e) {
+      logger_config_1.default.info(
+        `The connection to database was failed with error: ${e}`,
+      );
     }
-    catch (e) {
-        console.log(e)
-        logger_config_1.default.info(`The connection to database was failed with error: ${e}`);
-    }
-});
+  });
 connect();
